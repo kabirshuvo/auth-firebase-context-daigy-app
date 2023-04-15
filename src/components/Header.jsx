c13feeccbb6bb85ad359e21345481a5f28d46c80
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-16 05:50:27 +0600

    LogOut Function added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProviders';
 
 const Header = () => {
+
+const {user, logOut} = useContext(AuthContext);
+
+const handleLogOut = () => {
+  logOut()
+  .then(()=>{
+
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
+
     return (
         <div>
            <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <div className="navbar-start">
+              <div className="dropdown">
+                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                  </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         
-        <li><Link to='/'>Home</Link></li>
-        <li tabIndex={0}>
-          <a className="justify-between">
-            User
-            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-          </a>
-          <ul className="p-2">
-            <li><Link to='/login'>LogIn</Link></li>
-            <li><Link to='/register'>Register</Link></li>
+                      <li><Link to='/'>Home</Link></li>
+                      <li tabIndex={0}>
+                      <a className="justify-between">
+                           User
+                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+                      </a>
+                  <ul className="p-2">
+                    <li><Link to='/login'>LogIn</Link></li>
+                    <li><Link to='/register'>Register</Link></li>
+                  </ul>
+                </li>
+              <li><Link to='/'>Home</Link></li>
           </ul>
-        </li>
-        <li><Link to='/'>Home</Link></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost normal-case text-xl">Auth Master</a>
+      </div>
+            <a className="btn btn-ghost normal-case text-xl">Auth Master</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
@@ -45,7 +59,15 @@ const Header = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Get started</a>
+    {
+      user ? 
+      <>
+      <span>{user.email}</span>
+      <button onClick={handleLogOut} className="btn btn-xs">Log Out</button>
+      </>
+       : <Link to='/login'><button className="btn btn-xs">Please LogIn</button></Link>
+    }
+    {/* <a className="btn">Get started</a> */}
   </div>
 </div>
 
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -17,10 +17,28 @@ const AuthProviders = ({children}) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () => {
+       return signOut(auth);
+    }
+
+    //*observe auth state change
+
+    useEffect(()=>{
+        const nonsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('auth state change', currentUser);
+            setUser(currentUser);
+        });
+
+        return()=>{
+            nonsubscribe();
+        }
+    },[])
+
     const authInfo = {
         user,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
 
     return (
